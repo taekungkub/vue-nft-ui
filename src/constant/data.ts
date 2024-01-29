2a853ee1aae835f8993a8512567c0182478a81f2
Kieattisak Suparit <tokimekiyume@hotmail.com>
2024-01-29 11:26:07 +0700

    fix: edit type

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -2,18 +2,18 @@ export const data = {
   id: " #37_",
   image: new URL("../assets/pen.png", import.meta.url).href,
   title: "PudgyPenguins",
-  eatimated_price: 1.013,
-  floor_price: 1.88,
+  eatimated_price: "1.013",
+  floor_price: "1.880",
   acquisition_price: "0.000",
-  acquisition_date: "Jan 9, 2022",
+  acquisition_date: "Jun 9, 2022",
 };
 
 export interface IData {
   id: string;
   image: string;
   title: string;
-  eatimated_price: number;
-  floor_price: number;
+  eatimated_price: string;
+  floor_price: string;
   acquisition_price: string;
   acquisition_date: string;
 }
